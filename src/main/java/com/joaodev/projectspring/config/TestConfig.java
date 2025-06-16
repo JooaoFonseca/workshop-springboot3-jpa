8b7e8fdfc96f3368708b91ac4cd097b69e236bf7
João Matheus <sfojoao.matheus04@gmail.com>
2025-06-16 17:51:05 -0300

    Product

--- src/main/java/com/joaodev/projectspring/config/TestConfig.java
+++ src/main/java/com/joaodev/projectspring/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.joaodev.projectspring.entities.Category;
 import com.joaodev.projectspring.entities.Order;
+import com.joaodev.projectspring.entities.Product;
 import com.joaodev.projectspring.entities.User;
 import com.joaodev.projectspring.entities.enums.OrderStatus;
 import com.joaodev.projectspring.repositories.CategoryRepository;
 import com.joaodev.projectspring.repositories.OrderRepository;
+import com.joaodev.projectspring.repositories.ProductRepository;
 import com.joaodev.projectspring.repositories.UserRepository;
 
 @Configuration
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -36,17 +41,22 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-        
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         User u1 = new User(null, "Maria Down", "maria@gmail.com", "989898898", "123456");
         User u2 = new User(null, "Joao Matheus", "joao@gmail.com", "7777778787", "123456");
-        
+
         Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2025-06-20T03:20:14Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2025-06-20T13:32:55Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-
         userRepository.saveAll(Arrays.asList(u1, u2));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
     }
---
+++ src/main/java/com/joaodev/projectspring/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.joaodev.projectspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaodev.projectspring.entities.Product;
+import com.joaodev.projectspring.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return  obj.get();
+    }
+
+}
